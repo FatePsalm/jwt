9e4ac18f180a37c0e16d6549120bf3b755ce51a8
FatePsalm <32047409+FatePsalm@users.noreply.github.com>
2019-06-01 01:04:54 +0800

    2

---
+++ myjwttoken/src/main/java/com/solace/myjwt/MyJwtApplication.java
@@ -0,0 +1,12 @@
+package com.solace.myjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyJwtApplication.class, args);
+    }
+}
